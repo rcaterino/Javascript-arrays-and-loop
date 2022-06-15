2608da4615d02d2af13e0696c400214f7367d9e7
Ricardo Fabio Caterino Polini <104625061+rcaterino@users.noreply.github.com>
2022-06-15 16:45:05 +0200

    PRACTICING ARRAYS

---
+++ 02.1-Access-and-retrieve/app.js
@@ -0,0 +1,8 @@
+//declaring the array
+var myArray = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday'];
+
+console.log(myArray[2]); //1. print the item here
+
+ myArray[4] = null; //2. change 'thursday'a value here to null
+
+ console.log(myArray[4]); //console.log(myArray[3]); //3. print the position of step 2
---
+++ 02.2-Retrieve-items/apps.js
@@ -0,0 +1,7 @@
+var arr = [4,5,734,43,45,100,4,56,23,67,23,58,45,3,100,4,56,23];
+//Instructions:
+// 1. Print on the console the 1st element of the array
+// 2. Print on the console the 4th element of the array
+//your code here
+console.log(arr[0]);
+console.log(arr[3]);
