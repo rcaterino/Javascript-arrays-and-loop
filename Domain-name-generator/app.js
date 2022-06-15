d2afa657e66a25538f868211a63a2005f1d9064e
Ricardo Fabio Caterino Polini <104625061+rcaterino@users.noreply.github.com>
2022-06-15 20:37:53 +0200

    Practicando

---
+++ Domain-name-generator/app.js
@@ -0,0 +1,14 @@
+var pronoun = ["the", "our"];
+var adj = ["great", "big"];
+var noun = ["jogger", "racoon"];
+var ext = [".com",".net",".us",".io"]
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+        for (let h=0;h<ext.length;h++){
+            console.log(pronoun[i] + adj[j] + noun[k]+ ext[h]);
+        }
+    }
+  }
+}
